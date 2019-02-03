7f5496c7e81fbd7c8cb8e82a96043b1f22b14e6a
yenvih <yen-vi.huynh@mail.mcgill.ca>
2019-02-02 19:23:31 -0500

    initial

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
@@ -1,7 +1,7 @@
 package ca.mcgill.ecse321.eventregistration;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,7 +12,6 @@ public class EventRegistrationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EventRegistrationApplication.class, args);
 	}
-
   @RequestMapping("/")
   public String greeting(){
     return "Hello world!";
